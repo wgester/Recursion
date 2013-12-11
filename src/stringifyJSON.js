fd0a29b44f4776cb2aee336469441cbc47203d57
Will Gester <wiilgester@gmail.com>
2013-12-10 23:06:28 -0800

    Added base cases for strings and arrays and recursive methods for arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,34 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	if (typeof(obj) === "string"){
+		return "\"" + (obj) + "\"";
+	} else if (typeof(obj) === "number"){
+		return "" + obj + "";
+	} else {
+		return stringifyJSONhelper(obj);
+	}
+};
+
+var stringifyJSONhelper = function(obj){
+	if (Array.isArray(obj)){
+		if (obj.length === 0){
+			return "[]";
+		} else {
+			var helperString = "";
+			for (var i = 0; i < obj.length; i ++){
+				if (i === 0){
+					helperString = "[" + stringifyJSONhelper(obj[i]);
+				} else {
+					helperString = helperString + "," + stringifyJSONhelper(obj[i]);
+				}
+			}
+		}
+		var helperString = helperString + "]";
+		return helperString;
+	} else if (typeof(obj) === "string"){
+		return "\"" + obj + "\"";
+	} else if (typeof(obj) === "number"){
+		return obj;
+	}
 };
