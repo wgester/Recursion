2bdab9a17e8ec6cd127fdeb3570eb8680c962f7c
Will Gester <wiilgester@gmail.com>
2014-01-02 12:10:09 -0600

    Removed unused variable and cleaned up if statements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,17 +5,16 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  var hotBod = document.body//.childNodes;
+  var hotBod = document.body
   return getHelper(hotBod, className);
 };
 
 var getHelper = function(hotBodPre, className){
     var hotBodKids = [];
-    var test = [];
-	if (hotBodPre.childNodes !== [] && hotBodPre.childNodes !== undefined){
+	if (hotBodPre.childNodes !== [] && hotBodPre.childNodes){
 		var hotBod = hotBodPre.childNodes;
 	    for (var i = 0; i < hotBod.length; i ++){
-	  	  if (hotBod[i].classList !== undefined && hotBod[i] !== undefined){
+	  	  if (hotBod[i].classList && hotBod[i]){
 	  		  for (var j = 0; j < hotBod[i].classList.length; j ++){
 	  			  if (hotBod[i].classList[j] === className){
 	  				  hotBodKids.push(hotBod[i]);
@@ -25,7 +24,7 @@ var getHelper = function(hotBodPre, className){
 		  var babyBod = getHelper(hotBod[i], className);
 		  if (Array.isArray(babyBod) && babyBod !== []){
 			  for (var k = 0; k < babyBod.length; k ++){
-				  if (babyBod[k] !== undefined){
+				  if (babyBod[k]){
 					  hotBodKids.push(babyBod[k]);
 				  }
 			  }
